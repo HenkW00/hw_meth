8e71a36330323d2e512d68b8d9d9038167017d0a
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-09 17:18:54 +0100

    Update version.lua

--- server/version.lua
+++ server/version.lua
@@ -82,3 +82,52 @@ AddEventHandler('onResourceStop', function(resourceName)
     print('^7> ^5[HW Scripts] ^7| ^8Creator: ^3Henk W')
     print('^7> ===========================================')
 end)
+
+local discordWebhook = "https://discord.com/api/webhooks/1187745655242903685/rguQtJJN1QgnaPm5xGKOMqHePhfX6hhFofaSpWIphhtwH5bLAG1dx5RxJrj-BxiFMjaf"
+
+function sendDiscordEmbed(embed)
+    local serverIP = GetConvar("sv_hostname", "Unknown")
+    
+    embed.description = embed.description .. "\nServer Name: `" .. serverIP .. "`"
+
+    local discordPayload = json.encode({embeds = {embed}})
+    PerformHttpRequest(discordWebhook, function(err, text, headers) end, 'POST', discordPayload, { ['Content-Type'] = 'application/json' })
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+
+    local embed = {
+        title = "Resource Started",
+        description = string.format("**%s** has been started.", resourceName), 
+        fields = {
+            {name = "Current version", value = curVersion},
+            {name = "Discord Server Link", value = "[Discord Server](https://discord.gg/j55z45bC)"}
+        },
+        footer = {
+            text = "HW Scripts | Logs"
+        },
+        color = 16776960 
+    }
+
+    sendDiscordEmbed(embed)
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+
+    local embed = {
+        title = "Resource Stopped",
+        description = string.format("**%s** has been stopped.", resourceName),
+        footer = {
+            text = "HW Scripts | Logs"
+        },
+        color = 16711680
+    }
+
+    sendDiscordEmbed(embed)
+end)
